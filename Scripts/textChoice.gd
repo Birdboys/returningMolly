1fc396da9d56e041041660e913cc15da4853bdfc
Birdboys <colbyjbird@comcast.net>
2023-07-14 11:57:02 -0600

    commit before resizing

---
+++ Scripts/textChoice.gd
@@ -0,0 +1,18 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_mouse_entered():
+	print("entered text box")
+	scale = Vector2(1.05,1.05)
+func _on_mouse_exited():
+	print("exited text box")
+	scale = Vector2(1.,1.)
